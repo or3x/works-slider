048664c94b1db404cbecc1b5b93af984d1dafd00
Or3x <andrew_gomez@ukr.net>
2017-10-06 12:15:05 +0300

    done

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+
+$('.ba-slider').slick({
+	// arrows: true,
+	// 'dots': true,
+  // infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  // centerMode: true,
+  variableWidth: true
+  // 'nextArrow': '<a href="#" class="ba-slider__button ba-slider__button--next">Next</a>',
+  // 	'prevArrow': '<a href="#" class="ba-slider__button ba-slider__button--prev">Prev</a>'
+});
+
+});
+
+
